5c9c2670ea0d3552906f5ee4adf6ac09a8abd89d
MazinIsmailW <MA20011300@APL-31R4KW2>
2020-11-27 09:52:23 +0530

    NoSQL

---
+++ SpringBootMongoDB/src/main/java/com/db/learnings/DeploymentOptionsNoSQL.java
@@ -0,0 +1,23 @@
+package com.db.learnings;
+
+/**
+ * Deployment Options
+ * 
+ * While the public cloud has become an exciting and convenient way to deploy a
+ * database as a service (DBaaS), most computing still takes place on-premise.
+ * 
+ * Having the flexibility to deploy on-premise or in the cloud in a self-managed
+ * way as well as via a DBaaS model on any public cloud provides the ultimate
+ * flexibility.
+ * 
+ * In addition, many users of NoSQL databases will want full commercial support.
+ * 
+ * Each NoSQL database has a different combination of such offerings.
+ * 
+ * MongoDB supports all deployment models, self-managed on premise or in the
+ * cloud and DBaaS on all public clouds. MongoDB offers commercial support for
+ * its enterprise edition and for MongoDB Atlas, its DBaaS.
+ */
+public class DeploymentOptionsNoSQL {
+
+}
